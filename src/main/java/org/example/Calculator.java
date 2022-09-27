0d4d767a7515e8297ffad5211a61188f111ddec7
Rodrigo Guerrero <rodrigo.guerrero@solera.com>
2022-09-27 21:38:39 +0200

    Add addOddNumbers method

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 public class Calculator {
 
-    public int addListNumbers(List<Integer> list) {
-        return list.stream().reduce(0, Integer::sum);
+    public int addListNumbers(List<Integer> numbers) {
+        return numbers.stream().reduce(0, Integer::sum);
     }
+
+    public int addOddNumbers(List<Integer> numbers) {
+        return numbers.stream().filter(x -> x % 2 != 0).reduce(0, Integer::sum);
+    }
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,8 +8,13 @@ public class Main {
 
         Calculator calculator = new Calculator();
         List<Integer> numbers = List.of(10, 20, 30, 40, 50);
+        List<Integer> numbers2 = List.of(1, 2, 3, 4, 5, 1);
+
 
         int result = calculator.addListNumbers(numbers);
+        System.out.println(result);
+
+        result = calculator.addOddNumbers(numbers2);
         System.out.println(result);
 
     }
